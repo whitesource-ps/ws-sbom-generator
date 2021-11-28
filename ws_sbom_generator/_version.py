0df30ffdbdb6222a595fa5df9f1426687567afa6
tamari-oz <72387668+tamari-oz@users.noreply.github.com>
2021-11-28 11:43:28 +0100

    Minor fixes (#39)
    * Advanced PyPi Test
    
    * Fix CI
    
    * Standardizing package structure
    
    * Cosmetics
    
    * Added check if SPDX is not installed
    
    * Changed default report path (to better handle PyPi package)
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes

--- ws_sbom_generator/_version.py
+++ ws_sbom_generator/_version.py
@@ -1,4 +1,4 @@
 import os
-__version__ = os.environ.get("VERSION", "0.0.0-dev")
+__version__ = os.environ.get("VERSION", "0.0.0.dev0")
 __tool_name__ = "sbom_generator"
 __description__ = "WS SBOM Generator in SPDX format"
